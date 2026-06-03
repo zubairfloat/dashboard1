a81b461728768baf9aea51c39e8fe1bdeb740ece
Zubair Rizwan <zubairfloat@gmail.com>
2026-06-03 20:43:37 +0500

    resolve the issue

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,6 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
 
+const supabaseUrl =
+  process.env.NEXT_PUBLIC_SUPABASE_URL || "";
+
+const supabaseAnonKey =
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
+
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  supabaseUrl,
+  supabaseAnonKey
 );
